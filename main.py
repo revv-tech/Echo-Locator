8f7e52b4e69f465dc150afb93cba224b8b0409c2
Sven2301 <svegra01@gmail.com>
2020-11-07 15:39:15 -0600

    Trace

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def main():
     border_on = True
     num_walls = 3
     segment = 0
-    num_rays = 2
+    num_rays = 10
     ### END CONFIG
 
     pg.init()
@@ -64,34 +64,9 @@ def main():
         boundaries.append(Boundary(screen, (screen_w, screen_h), (0, screen_h)))
         boundaries.append(Boundary(screen, (0, screen_h), (0, 0)))
 
-    #BOUNDERIES
-    boundaries.append(Boundary(screen, (20, 85), (50, 30)))
-    boundaries.append(Boundary(screen, (50, 30), (100, 30)))
-    boundaries.append(Boundary(screen, (250, 30), (200, 30)))
-    boundaries.append(Boundary(screen, (250, 30), (300, 10)))
-    boundaries.append(Boundary(screen, (10, 200), (50, 230)))    
-    boundaries.append(Boundary(screen, (450, 30), (495, 70)))
-    boundaries.append(Boundary(screen, (350, 30), (400, 20)))
-    boundaries.append(Boundary(screen, (495, 70), (495, 200)))
-    boundaries.append(Boundary(screen, (495, 70), (495, 200)))
-    boundaries.append(Boundary(screen, (495, 200), (400, 250)))
-    boundaries.append(Boundary(screen, (495, 70), (495, 200)))
-    boundaries.append(Boundary(screen, (320, 70), (270, 100)))
-    
-    boundaries.append(Boundary(screen, (495, 495), (400,495)))
-    boundaries.append(Boundary(screen, (300, 400), (400,495)))
-    boundaries.append(Boundary(screen, (495, 350), (400,320)))
-    boundaries.append(Boundary(screen, (495, 350), (450,400)))
-    boundaries.append(Boundary(screen, (240, 320), (300,300)))
-    boundaries.append(Boundary(screen, (190, 200), (150,200)))
-    boundaries.append(Boundary(screen, (20, 400), (50,350)))
-    boundaries.append(Boundary(screen, (20, 400), (60,370)))
-    boundaries.append(Boundary(screen, (60, 370), (60,476)))
-    boundaries.append(Boundary(screen, (80, 400), (60,476)))
-    boundaries.append(Boundary(screen, (150, 390), (150,450)))
-    boundaries.append(Boundary(screen, (300, 485), (150,450)))
-    boundaries.append(Boundary(screen, (90, 300), (50,250)))
-                      
+    for i in range(num_walls):
+        boundaries.append(Boundary(screen, (300, 300), (400, 300)))
+ 
     while running:
      
         for event in pg.event.get():
@@ -102,7 +77,7 @@ def main():
                 if event.key == pygame.K_BACKSPACE:
                     rays = rayEditor(segment,num_rays,p)
                 #CAMBIAN LOS SECTORES
-                if event.key == pygame.K_4:
+                if event.key == pygame.K_0:
                     segment = 0
                     rays = rayEditor(segment,num_rays,p)
                 if event.key == pygame.K_1:
--- ray.py
+++ ray.py
@@ -17,8 +17,8 @@ class Ray:
 
     def update(self, screen: pg.display, p: Particle, boundaries: list):
 
-        colorP = (100,100,100)
-        colorS = (100, 50, 100)
+        colorP = (0,0,0)
+        colorS = (0, 0, 0)
         self.start = p.pos
         #print(p.pos)
         self.end.from_polar((10000, self.heading))
@@ -62,52 +62,8 @@ class Ray:
             if self.flagPrimary:
                 
                 self.image = drawline(screen, colorP, self.start, self.end, 1) # COLOR
+                drawline(screen, pg.Color('white'), self.end, self.end, 1) # COLOR
                 
             else:
                 self.image = drawline(screen, colorS, self.start, self.end, 1) # COLOR
-
-    
-a1 = decimal.Decimal(input('a1: '))
-b1 = decimal.Decimal(input('b1: '))
-c1 = decimal.Decimal(input('c1: '))
- 
-a2 = decimal.Decimal(input('a2: '))
-b2 = decimal.Decimal(input('b2: '))
-c2 = decimal.Decimal(input('c2: '))
-def pinta_ecuacion(a, b, c):
-    dev = ''
-    if a != 0:
-        dev += '%sx' % a
-    if b > 0:
-        dev += ' + %sy ' % b
-    elif b < 0:
-        dev += ' %sy ' % b
-    dev += ' = %s' % c
- 
-    return dev
- 
-print(pinta_ecuacion(a1, b1, c1))
-print(pinta_ecuacion(a2, b2, c2))
- 
-numerador_y = a2*c1 - a1*c2
-denominador_y = a2*b1 - a1*b2
- 
-# método ce cálculo de la posición de rectas:
-paralelas = a1*b2 == a2*b1
-if paralelas:
-    coincidentes = a1*c2 == a2*c1
-    if coincidentes:
-        print('Rectas coincidentes')
-    else:
-        print('Rectas paralelas')
-else:
-    # son secantes. calculo el punto de intersección
-    if a1 == 0:
-        y = c1/b1
-        x = (c2-b2*y)/a2
-    else:
-        y = numerador_y/denominador_y
-        x = (c1-b1*y)/a1
- 
-    print('Punto intersección: (%s, %s)' % (x, y))
     
